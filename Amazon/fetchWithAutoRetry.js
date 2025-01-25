7a6bda8231fb963ab563a6f7fc896b9f881987be
mudassir-jmi <mudassir7991@gmail.com>
2025-01-25 21:41:39 +0530

    add question on fetchwithautoretry

--- Amazon/fetchWithAutoRetry.js
+++ Amazon/fetchWithAutoRetry.js
@@ -1,3 +1,13 @@
+/*
+The API calls might fail because of Network problems. Usually we could show a screen for Network Error and ask users to retry.
+
+One approach to handle this is auto retry when network error occurs.
+
+You are asked to create a fetchWithAutoRetry(fetcher, count), which automatically fetch again when error happens, until the maximum count is met.
+
+For the problem here, there is no need to detect network error, you can just retry on all promise rejections.
+*/
+
 function fetchWithAutoRetry(fetcher, count) {
     return new Promise((resolve, reject) => {
       let attempts = 0;
